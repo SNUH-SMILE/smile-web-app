7c5b3656edc3be5d0b3e791404f95d9c4e28f415
최우영 <dndud159@hconnect.co.kr>
2022-05-27 14:59:01 +0900

    SMILE-80 FRONT Axios Token 관리 기능      - Api 요청시 LocalStorage에 저장된 토큰을 Axios Config 에 추가하도록 구현

--- src/Hooks/AuthorizationAxios.js
+++ src/Hooks/AuthorizationAxios.js
@@ -4,4 +4,12 @@ const AuthorizationAxios = axios.create({
   baseURL:process.env.REACT_APP_BASE_URL
 })
 
+// 요청전에 LocalStorage 에 담겨 있는 토큰 헤더에 세팅
+AuthorizationAxios.interceptors.request.use(
+    function (config) {
+        config.headers={'Authorization':`Bearer ${localStorage.getItem('Authorization')}`}
+      return config;
+    }
+)
+
 export default AuthorizationAxios;
