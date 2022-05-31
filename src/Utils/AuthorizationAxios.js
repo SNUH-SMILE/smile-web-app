357ea366a599b29a3afd5d9c653d045a7ced0bee
최우영 <dndud159@hconnect.co.kr>
2022-05-31 16:19:25 +0900

    SMILE-80 AuthorizationAxios FIX 요청시 헤더에 토큰을 추가하면서 기존에 추가한 헤더값이 삭제되는 이슈 수정

--- src/Utils/AuthorizationAxios.js
+++ src/Utils/AuthorizationAxios.js
@@ -7,7 +7,7 @@ const AuthorizationAxios = axios.create({
 // 요청전에 LocalStorage 에 담겨 있는 토큰 헤더에 세팅
 AuthorizationAxios.interceptors.request.use(
     function (config) {
-        config.headers={'Authorization':`Bearer ${localStorage.getItem('Authorization')}`}
+        config.headers={'Authorization':`Bearer ${localStorage.getItem('Authorization')}`,...config.headers}
       return config;
     }
 )
