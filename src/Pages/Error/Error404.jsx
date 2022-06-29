09f9ba831a6211530368c1bde3c53f37a67dbf76
최우영 <dndud159@hconnect.co.kr>
2022-06-29 11:01:55 +0900

    SMILE-103 응답결과가 정상이 아닌경우 페이지 처리          - Response Status 가 500 인 경우 Error 페이지로이동

--- src/Pages/Error/Error404.jsx
+++ src/Pages/Error/Error404.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function
-
-Error404(props)
+function Error404(props)
 {
+    const moveMainPage = () =>{
+        window.location='/';
+    }
     return (
-        <h1>404</h1>
+        <>
+            <h1>404</h1>
+            <button onClick={moveMainPage}>메인페이지로 돌아가기</button>
+        </>
     );
 }
 
--- src/Utils/AuthorizationAxios.js
+++ src/Utils/AuthorizationAxios.js
@@ -4,12 +4,33 @@ const AuthorizationAxios = axios.create({
   baseURL:process.env.REACT_APP_BASE_URL
 })
 
+
+
 // 요청전에 LocalStorage 에 담겨 있는 토큰 헤더에 세팅
 AuthorizationAxios.interceptors.request.use(
     function (config) {
         config.headers={'Authorization':`Bearer ${localStorage.getItem('Authorization')}`,...config.headers}
       return config;
+    },
+    error => {
+        console.log('Request',error)
     }
+)
+
+AuthorizationAxios.interceptors.response.use(
+
+    (response) => {
+
+        // console.log('response',response);
+        return response;
+    },
+    (error => {
+        console.log('ResponseError',error)
+        if(error.response.status === 500){
+            window.location='/error';
+        }
+    })
+
 )
 
 export default AuthorizationAxios;
