423d10b79ac2924f011dd83c173bfdbb66ba6cbb
SeonDahyee <seonn511@gmail.com>
2023-09-18 10:36:38 +0900

    feat: 위치정보 데이터 조회

--- src/component/LocationModal.jsx
+++ src/component/LocationModal.jsx
@@ -1,66 +1,93 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import { Modal } from "react-bootstrap";
-import InterviewList from "./InterviewList";
+import PatientLocationsApi from "../Apis/PatientLocationsApi";
+import getToday from "../Utils/common";
 
 function LocationModal({ locationModal, handledClose }) {
-    const test =
-        [{
-        'admissionId' : "0000000079",
-        'cdata': null,
-        'infDiv' :null,
-        'infDivNm' : "체온상승",
-        'message': "체온상승 예측 알고리름 id=0000000079 & 일자=2023-01-10 & 생체데이터=  호흡   데이터가 없습니다."
-          }
-        ]
-    const [inference , setInference] = useState([]);
-    useEffect(() =>{
-        setInference(locationModal.data);
+  const todayInput = useRef();
+  const [resultDate, setResultDate] = useState("");
+  const [locationData, setLocationData] = useState([]);
+  const locationApi = new PatientLocationsApi();
 
-    })
-    return(
-        <Modal show={locationModal.show}
-               onHide={() => handledClose()}
-               className={'locationModal'}
-               dialogClassName={'modal-dialog modal-dialog-centered modal-dialog-scrollable'}
+  useEffect(() => {
+    setLocationData(locationModal.data);
+    if (locationModal.show) {
+      todayInput.current.value = getToday();
+    }
+  }, [locationModal.show]);
+
+  useEffect(() => {
+    locationApi
+      .update(locationModal.admissionId, resultDate)
+      .then(({ data }) => {
+        setLocationData({ ...data });
+      });
+  }, [resultDate]);
+
+  function onChangeDate(e) {
+    console.log(e.target.value);
+    setResultDate(e.target.value);
+    console.log(`set : ${resultDate}`);
+    console.log(locationModal.admissionId);
+  }
+  return (
+    <Modal
+      show={locationModal.show}
+      onHide={() => handledClose()}
+      className={"locationModal"}
+      dialogClassName={
+        "modal-dialog modal-dialog-centered modal-dialog-scrollable"
+      }
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{locationData.patientNm}/자택격리자</Modal.Title>
+        <span className="divideLine">|</span>
+        <span className="modalSubTitle">생년월일</span>
+        <span className="headerText">
+          {locationData.birthDate} ({locationData.age}/{locationData.sex})
+        </span>
+        <span className="modalSubTitle">환자번호</span>
+        <span className="headerText">{locationData.patientId}</span>
+        <span className="modalSubTitle">연락처</span>
+        <span className="headerText">{locationData.cellPhone}</span>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="dateContainer">
+          <span className="inputLabel">측정일</span>
+          <input
+            className="form-control date inputWidth"
+            type="date"
+            ref={todayInput}
+            required
+            onChange={onChangeDate}
+          />
+        </div>
+        <div
+          className="table-responsive"
+          style={{ height: "600px", overflow: "scroll" }}
         >
-            <Modal.Header closeButton>
-                <Modal.Title>이*민/자택격리자</Modal.Title>
-                <span className="divideLine">|</span>
-                <span className="modalSubTitle">생년월일</span>
-                <span className='headerText'>1988-08-17 (35/F)</span>
-                <span className="modalSubTitle">환자번호</span>
-                <span className='headerText'>P000000155</span>
-                <span className="modalSubTitle">연락처</span>
-                <span className='headerText'>010****7393</span>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="dateContainer">
-                    <span className="inputLabel">측정일</span>
-                    <input className="form-control date inputWidth" type="date" />     
-                </div>
-                <div className="table-responsive" style={{height:'600px',overflow :"scroll"}}>
-                    <table className="table table-borderless mt-3 alignCenter tHeader">
-                        <thead>
-                            <tr>
-                                <th>측정 시간</th>
-                                <th>위도</th>
-                                <th>경도</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {inference.length >0 && inference.map((value,idx)=>(
-                                <tr>
-                                    <td>{value.cdate}</td>
-                                    <td>{value.infDivNm}</td>
-                                    <td>{value.message.substring(value.message.lastIndexOf('=')+1)}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </Modal.Body>
-
-        </Modal>
-    )
+          <table className="table table-borderless mt-3 alignCenter tHeader">
+            <thead>
+              <tr>
+                <th>측정 시간</th>
+                <th>위도</th>
+                <th>경도</th>
+              </tr>
+            </thead>
+            <tbody>
+              {locationData.patientLocations &&
+                locationData.patientLocations.map((value, idx) => (
+                  <tr>
+                    <td>{value.resultDt}</td>
+                    <td>{value.latitude}</td>
+                    <td>{value.longitude}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 }
 export default React.memo(LocationModal);
